1831e50674fd716a4f373ba178a0807acddec93f
termie <github@anarkystic.com>
2014-06-10 16:19:01 +0200

    update description for pypi

--- setup.py
+++ setup.py
@@ -1,9 +1,17 @@
 from setuptools import setup
 
+# Utility function to read the README file.
+# Used for the long_description.  It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
+def read(fname):
+  return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
 setup(
   name='govenv',
   version='0.1',
-  description = 'Golang Enhancements to virtualenv',
+  description='Golang Enhancements to virtualenv',
+  long_description=read('README.rst'),
   url='https://github.com/termie/govenv',
   packages=['govenv'],
   author='Andy Smith',
